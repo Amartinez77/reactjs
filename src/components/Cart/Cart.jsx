aef5250e8fda686a71f9f1018f73c9a0217a0e82
Ariel Martinez <ariel.mtz85@gmail.com>
2022-08-15 23:27:33 -0300

    desafio Item Collection II

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,49 +1,115 @@
-import React from 'react'
-import { useCartContext } from '../../Context/CartContext'
-import ButttonSeguirCompra from '../ButttonSeguirCompra/ButttonSeguirCompra'
-import CartEmpty from './CartEmpty'
+import React, { useState } from "react";
+import { useCartContext } from "../../Context/CartContext";
+import ButttonSeguirCompra from "../ButttonSeguirCompra/ButttonSeguirCompra";
+import CartEmpty from "./CartEmpty";
+import {
+  addDoc,
+  collection,
+  doc,
+  getFirestore,
+  updateDoc,
+} from "firebase/firestore";
 
 const Cart = () => {
+  const { cartList, vaciarCarrito, eliminarXunidad, precioTotal } =
+    useCartContext();
+  const [id, setId] = useState("");
+  const [date, setDate] = useState({});
+  // console.log(cartList);
 
-  const { cartList, vaciarCarrito, eliminarXunidad, precioTotal } = useCartContext()
-  console.log(cartList);
-  
-  
+  // funcion que guarda la orden de compra
+  const sendOrder = (e) => {
+    e.preventDefault();
+
+    const order = {};
+    order.buyer = {
+      email: "pepe@gmail.com",
+      name: "pepe",
+      phone: "38884516161",
+    };
+
+    order.items = cartList.map((prod) => {
+      return {
+        product: prod.brand,
+        id: prod.id,
+        price: prod.price,
+      };
+    });
+
+    order.date = new Date();
+    order.total = precioTotal();
+
+    // console.log(order);
+    // guardar orden en la base de datos
+
+    const db = getFirestore();
+    const queryOrders = collection(db, "orders");
+    addDoc(queryOrders, order)
+      .then((resp) => setId(resp.id))
+      .catch((err) => console.log(err));
+
+    // actualizar un documento
+
+    //   const queryUpdateDoc = doc(db, 'products', 'A59XJcDJSMZIW1GPk949')
+    //   updateDoc(queryUpdateDoc, {
+    //       stock: 29
+    //   })
+    //   .then(()=> console.log('Item actualizado'))
+  };
+
   return (
     (cartList.length > 0 && (
       <div className="container">
         <h2>Cart</h2>
-        <ul>
-          {cartList.map((item) => (
-            <li key={item.id}>
-              <div className="w-25">
-                {console.log(item.cantidad)}
-                <img
-                  src={item.imagePath}
-                  alt="Foto de producto"
-                  className="w-25"
-                />
-                nombre: {item.brand} - cantidad {item.cantidad} {item.price} -
-                Subtotal: {item.cantidad * item.price}
-              </div>
-              <button
-                onClick={() => {
-                  eliminarXunidad(item.id);
-                }}>
-                {" "}
-                X{" "}
-              </button>
-            </li>
-          ))}
-        </ul>
-        <div><h4>total: <span>{ precioTotal() }</span></h4></div>
-        <button className='btn btn-danger' onClick={vaciarCarrito}>Vaciar todo el carrito</button>
-        <br/>
-        <ButttonSeguirCompra />
-      </div>
-    ))
-    || <CartEmpty />
-  );
-}
+        {id.length > 0 ? (
+          <>
+            <h2>El id de la orden es: {id}</h2>            
+            <h3>gracias por su compra</h3>
+          </>
+        ) : (
+          <ul>
+            {cartList.map((item) => (
+              <li key={item.id}>
+                <div className="w-25">
+                  {console.log(item.cantidad)}
+                  <img
+                    src={item.imagePath}
+                    alt="Foto de producto"
+                    className="w-25"
+                  />
+                  nombre: {item.brand} - cantidad {item.cantidad} {item.price} -
+                  Subtotal: {item.cantidad * item.price}
+                </div>
+                <button
+                  onClick={() => {
+                    eliminarXunidad(item.id);
+                  }}>
+                  {" "}
+                  X{" "}
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
 
-export default Cart
+        <div>
+          <h4>
+            total: <span>{precioTotal()}</span>
+          </h4>
+        </div>
+        <button className="btn btn-danger" onClick={vaciarCarrito}>
+          Vaciar todo el carrito
+        </button>
+
+        <br />
+        <ButttonSeguirCompra />
+        <br />
+        <button className="btn btn-danger" onClick={sendOrder}>
+          enviar orden de compra
+        </button>
+      </div>
+    )) || <CartEmpty />
+  );
+};
+
+export default Cart;
