88542d5f37900e62328f4fed601a318aeaa35e4e
Ariel Martinez <ariel.mtz85@gmail.com>
2022-08-31 00:31:07 -0300

    proyecto final react

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,6 +8,10 @@ const Footer = () => {
       {/* <div className="container"> */}
       <footer className="py-3 my-4">
         <p className="text-center text-muted">
+          Prohibida la venta de bebidas alcoholicas a menores de 18 años - Recuerda
+          beber con moderación 
+        </p>
+        <p className="text-center text-muted">
           &copy; Proyecto de E-commerce realizado para el curso de React en
           Coderhouse.
         </p>
